3659289f4f18b7382624e0a60e08c93d0ea2e543
Mark Regalla <regalla.mark@gmail.com>
2015-06-02 22:03:59 -0400

    add revisions Damian showed me 6-2-2015

--- temperature.py
+++ temperature.py
@@ -12,25 +12,36 @@ import time  # a package with datetime objects
 from dateutil.parser import parse  # a package for parsing a string into a Python datetime object
 import collections
 import datetime
-#import odict #imports ordered dictionary library
+#from odict import odict #imports ordered dictionary library
 
-cities = { "Atlanta": '33.762909, -84.422675',
-            "Austin": '30.303936, -97.754355',
-            "Boston": '42.331960, -71.020173',
-            "Chicago": '41.837551, -87.681844',
-            "Cleveland": '41.478462, -81.679435'
+cities = { "Atlanta": '33.762909,-84.422675',
+           "Austin": '30.303936,-97.754355',
+           "Boston": '42.331960,-71.020173',
+           "Chicago": '41.837551,-87.681844',
+           "Cleveland": '41.478462,-81.679435'
         }
-#cities = OrderedDict(sorted(cities.items(), key=lambda t: t[0]))
 
 print cities
-'''
-cities = pd.DataFrame(cities.items())
+
+cities = pd.DataFrame(cities.items(), columns=['city', 'coordinates'])
+cities.sort(columns=['city'], ascending=True, inplace=True)
+cities.reset_index(inplace=True)
+cities.drop('index', axis=1, inplace=True)
 print cities
 
-#print cities.iloc[0,0]
+base_url = 'https://api.forecast.io/forecast/ed5384712a6c42598d505ff33c0bd2aa/'
+#start_date = datetime.datetime.now() - datetime.timedelta(days=30)
+start_date = datetime.datetime.now()
+start_date = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%dT%H:%M:%S%z')
+#print start_date
 
-start_date = datetime.datetime.now() - datetime.timedelta(days=30)
+#data_list[]
+for row in cities.iterrows():
+	url = base_url + row[1]['coordinates'] + ',' + start_date	#row is a 2-dimensional variable, containing city and coordinates for each row
+	r = requests.get(url)
+	print r.status_code		#prints url status.  If 400, then invalid request was made and will not fetch the data
+	print r.url
+	max_temp = r.json()['daily']['data'][0]['temperatureMax']
+	print max_temp
 
-#for i in cities
-	#r[i] = requests.get('https://api.forecast.io/forecast/ed5384712a6c42598d505ff33c0bd2aa/') #not done yet
-'''
+	#make a table of city, date, max temp
